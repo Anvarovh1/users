b2e79661708a4369e17c05442da78e9e2b082d55
anvarov_h <anvarovhumoyunmirzo1@gmail.com>
2023-01-03 18:05:06 +0500

    users

---
+++ main.js
@@ -0,0 +1,23 @@
+const users = {
+    1: {
+        name: prompt(`1-Foydanauvching ismini kiriting`),
+        age: prompt(`1-Foydanauvching yoshini kiriting`)
+    },
+    2: {
+        name: prompt(`2-Foydanauvching ismini kiriting`),
+        age: prompt(`2-Foydanauvching yoshini kiriting`)
+    },
+    3: {
+        name: prompt(`3-Foydanauvching ismini kiriting`),
+        age: prompt(`3-Foydanauvching yoshini kiriting`)
+    },
+}
+for (const key in users){
+    for (const key2 in users[key]) {
+        let user1 = `Foydanauvchi 1  \nsiznig ismingiz ${users[1].name} \nsizning yoshingiz ${users[1].age}`
+        let user2 = `\nFoydanauvchi 2  \nsiznig ismingiz ${users[2].name} \nsizning yoshingiz ${users[2].age}`
+        let user3 = `\nFoydanauvchi 3  \nsiznig ismingiz ${users[3].name} \nsizning yoshingiz ${users[3].age}`
+        console.log(user1,user2,user3)
+    }
+}
+console.log(users)
